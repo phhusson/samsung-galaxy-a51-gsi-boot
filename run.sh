4c2a3f030bc2550de68797d07dff0349b1dfc5e6
Pierre-Hugues Husson <phh@phh.me>
2021-12-21 13:46:10 +0100

    Fix syntax

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ cd d
 ~phh/Downloads/magisk/x86/magiskboot unpack ../r.img
 ~phh/Downloads/magisk/x86/magiskboot cpio ramdisk.cpio extract
 # Reverse fastbootd ENG mode check
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 20 01 00 35
+~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 # 20 01 00 35
 ~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
 ~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
 ~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 50c0009420010034 50c0009420010035
