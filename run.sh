90213ad74155703bee0a7fb2fd29db8622dda556
Pierre-Hugues Husson <phh@phh.me>
2023-08-01 22:15:01 +0200

    Rewrite magiskboot path, add a new 32bits hexpatch

--- run.sh
+++ run.sh
@@ -21,23 +21,27 @@ rm -Rf d
 (
 mkdir d
 cd d
-~phh/Downloads/magisk/x86/magiskboot unpack ../r.img
-~phh/Downloads/magisk/x86/magiskboot cpio ramdisk.cpio extract
+../magiskboot unpack ../r.img
+../magiskboot cpio ramdisk.cpio extract
 # Reverse fastbootd ENG mode check
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 # 20 01 00 35
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 50c0009420010034 50c0009420010035
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 080109aae80000b4 080109aae80000b5
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 20f0a6ef38b1681c 20f0a6ef38b9681c
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 23f03aed38b1681c 23f03aed38b9681c
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 20f09eef38b1681c 20f09eef38b9681c
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 26f0ceec30b1681c 26f0ceec30b9681c
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
-~phh/Downloads/magisk/x86/magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
-~phh/Downloads/magisk/x86/magiskboot cpio ramdisk.cpio 'add 0755 system/bin/recovery system/bin/recovery'
-~phh/Downloads/magisk/x86/magiskboot repack ../r.img new-boot.img
+set +e
+../magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 # 20 01 00 35
+../magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
+../magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
+../magiskboot hexpatch system/bin/recovery 50c0009420010034 50c0009420010035
+../magiskboot hexpatch system/bin/recovery 080109aae80000b4 080109aae80000b5
+../magiskboot hexpatch system/bin/recovery 20f0a6ef38b1681c 20f0a6ef38b9681c
+../magiskboot hexpatch system/bin/recovery 23f03aed38b1681c 23f03aed38b9681c
+../magiskboot hexpatch system/bin/recovery 20f09eef38b1681c 20f09eef38b9681c
+../magiskboot hexpatch system/bin/recovery 26f0ceec30b1681c 26f0ceec30b9681c
+../magiskboot hexpatch system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
+../magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
+../magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
+../magiskboot hexpatch system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
+
+set -e
+../magiskboot cpio ramdisk.cpio 'add 0755 system/bin/recovery system/bin/recovery'
+../magiskboot repack ../r.img new-boot.img
 cp new-boot.img ../r.img
 )
 
